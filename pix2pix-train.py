fde618a2d0561c834ef8c600e7f72687484d1370
Josh Murr <joshuadavidmurr@gmail.com>
2020-09-08 18:21:56 +0100

    User input stuff

--- pix2pix-train.py
+++ pix2pix-train.py
@@ -52,15 +52,39 @@ if __name__ == '__main__':
         print("Using name 'sample' as no name was provided.")
         print()
 
-    if args.dataset:
-        if os.path.exists(args.dataset) and os.path.isdir(args.dataset):
-            DATASET_PATH = args.dataset
-            print("Checking files in dataset directory...")
-            count = 0
-            extensions = set()
-            for file in os.listdir(DATASET_PATH):
-                count += 1
-                ext = os.path.splitext(file)[1]
-                extensions.add(ext)
+    dataset_count = 0
+    if args.dataset and os.path.exists(args.dataset)\
+            and os.path.isdir(args.dataset):
+        DATASET_PATH = args.dataset
+        print("Checking files in dataset directory...")
+        print()
+        extensions = set()
+        for file in os.listdir(DATASET_PATH):
+            dataset_count += 1
+            ext = os.path.splitext(file)[1]
+            extensions.add(ext)
+        print(f"Found {dataset_count} files with the extensions: {extensions}")
+        print()
 
-            print(f"Found {count} files with the extensions: {extensions}")
+    else:
+        print("Please provide a valid path to a directory containing the\
+              dataset.")
+        print()
+
+    if not args.batch_size:
+        print("No Batch Size provided, using default of 4.")
+        print()
+    else:
+        BATCH_SIZE = args.batch_size
+        print(f"Using provided Batch Size of {BATCH_SIZE}")
+        print()
+
+    if not args.buffer_size:
+        BUFFER_SIZE = dataset_count if dataset_count <= 2000 else 1000
+        print("No Buffer Size provided.")
+        print(f"There are {dataset_count} images in the dataset, " +
+              f"so using calculated Buffer Size of {BUFFER_SIZE}.")
+        print()
+    else:
+        BUFFER_SIZE = args.buffer_size
+        print(f"Using provided Buffer Size of {BUFFER_SIZE}")
