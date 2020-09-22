a57bf9251c57747ffb905c1eb71b8f63e138aec6
Josh Murr <joshuadavidmurr@gmail.com>
2020-09-22 13:27:28 +0100

    newaxis

--- model.py
+++ model.py
@@ -237,13 +237,13 @@ class Model:
         prediction = model(test_input, training=True)
         plt.figure(figsize=(15, 15))
 
-        display_list = [test_input, tar, prediction[0]]
+        display_list = [test_input[0], tar[0], prediction[0]]
         title = ['Input Image', 'Ground Truth', 'Predicted Image']
 
         for i in range(3):
             plt.subplot(1, 3, i + 1)
             plt.title(title[i])
-            plt.imshow(display_list[i] * 0.5 + 0.5)
+            plt.imshow(display_list[i][tf.newaxis, ...] * 0.5 + 0.5)
             plt.axis('off')
         plt.show()
 
