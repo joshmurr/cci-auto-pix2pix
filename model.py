29a44e12fd1b140e8447ad8884e4eaf0aaff8845
Josh Murr <joshuadavidmurr@gmail.com>
2020-09-22 13:36:15 +0100

    Reshape greyscale

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import time
 from matplotlib import pyplot as plt
 from IPython import display
 from helpers import getNumLayers
+from numpy import reshape
 
 
 class Model:
@@ -237,13 +238,13 @@ class Model:
         prediction = model(test_input, training=True)
         plt.figure(figsize=(15, 15))
 
-        display_list = [test_input[0], tar[0], prediction[0]]
+        display_list = [test_input[0][:, :, 0], tar[0], prediction[0]]
         title = ['Input Image', 'Ground Truth', 'Predicted Image']
 
         for i in range(3):
             plt.subplot(1, 3, i + 1)
             plt.title(title[i])
-            plt.imshow(display_list[i][tf.newaxis, ...] * 0.5 + 0.5)
+            plt.imshow(display_list[i] * 0.5 + 0.5)
             plt.axis('off')
         plt.show()
 
